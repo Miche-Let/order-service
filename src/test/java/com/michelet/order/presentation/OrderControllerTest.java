59fda25760d708f3d473d2e19d0091a6ada01ec3
ji-circle <sp202401jw@gmail.com>
2026-04-26 19:20:43 +0900

    fix: RestDocs uriPort = 19700 기록되게 수정

--- src/test/java/com/michelet/order/presentation/OrderControllerTest.java
+++ src/test/java/com/michelet/order/presentation/OrderControllerTest.java
@@ -16,7 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@AutoConfigureRestDocs
+@AutoConfigureRestDocs(uriScheme = "http", uriHost = "localhost", uriPort = 19700)
 @ActiveProfiles("test")
 @Import(RestDocsConfig.class)
 public class OrderControllerTest {
