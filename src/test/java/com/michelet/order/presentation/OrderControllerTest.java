2a47f3cd9ca2995e07f471480a10fcd12157f934
ji-circle <sp202401jw@gmail.com>
2026-04-26 18:19:18 +0900

    test: 헬스체크 테스트코드

---
+++ src/test/java/com/michelet/order/presentation/OrderControllerTest.java
@@ -0,0 +1,34 @@
+package com.michelet.order.presentation;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@ActiveProfiles("test")
+@Import(RestDocsConfig.class)
+public class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthCheck() throws Exception {
+        mockMvc.perform(get("/api/v1/orders/health")
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andDo(document("{class-name}/{method-name}")); // 명시적으로 추가
+    }
+}
