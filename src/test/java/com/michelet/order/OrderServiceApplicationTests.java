bf11a31537148692b437beafa37a3688ce468aa1
ji-circle <sp202401jw@gmail.com>
2026-04-29 03:34:27 +0900

    fix: 코드래빗 리뷰 반영 - 테스트 코드 수정

--- src/test/java/com/michelet/order/OrderServiceApplicationTests.java
+++ src/test/java/com/michelet/order/OrderServiceApplicationTests.java
@@ -1,15 +1,13 @@
 package com.michelet.order;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
