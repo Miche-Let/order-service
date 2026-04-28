dfddbbc4d826af380b54e901fd9d644cfe26c8c8
ji-circle <sp202401jw@gmail.com>
2026-04-28 23:12:25 +0900

    fix: 테스트코드 수정 - SpringBootTest 제거, Auditing

--- src/test/java/com/michelet/order/infrastructure/repository/OrderPersistenceTest.java
+++ src/test/java/com/michelet/order/infrastructure/repository/OrderPersistenceTest.java
@@ -1,18 +1,11 @@
 package com.michelet.order.infrastructure.repository;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.michelet.common.config.JpaAuditingConfig;
 import com.michelet.order.domain.model.Order;
 import com.michelet.order.domain.model.OrderItem;
 import com.michelet.order.domain.model.ReceivingMethod;
-import com.michelet.order.infrastructure.config.JpaConfig;
+import com.michelet.order.infrastructure.config.AuditorConfig;
 import jakarta.persistence.EntityManager;
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +13,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
-@Import({JpaConfig.class, JpaAuditingConfig.class})
+@Import({AuditorConfig.class, JpaAuditingConfig.class})
 @ActiveProfiles("test")
 class OrderPersistenceTest {
 
