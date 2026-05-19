e25ab3cc5605e418f0b3432b39cd7e15c2a5fa49
Jiwon-Choi <162451391+ji-circle@users.noreply.github.com>
2026-05-20 03:56:54 +0900

    chore/16 - fix: feign 코드 수정 (#40)
    * fix: feign 코드 수정
    
    * fix: 들여쓰기 수정

--- src/main/java/com/michelet/order/infrastructure/client/CatalogClient.java
+++ src/main/java/com/michelet/order/infrastructure/client/CatalogClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalog-service", url = "${client.catalog-service.url}")
+@FeignClient(name = "catalog-service")
 public interface CatalogClient {
     @GetMapping("/internal/options/{optionId}/validate")
     ApiResponse<OptionValidationResponse> validateOption(@PathVariable("optionId") UUID optionId);
